9e5fa4753beb7f02cc2d52ea6694f66445d1a13a
Mykola Sadlii <mykolac08@gmail.com>
2025-08-28 19:56:08 +0100

    Number Guessing Game

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# python-test
-playing with something that i ve learned before
+# Number Guessing Game
+
+**this is my first ever python project so it s not very cool**
+
+## TO LAUNCH THE GAME
+
+Clone this repository and open in Visual Studio code
+
+Open terminal and type **"python python.py"** or simply press the triangular button 
+
+## You have 5 attempts to guess the number 1 - 20
---
+++ python.py
@@ -0,0 +1,78 @@
+# print("Hello, world!")
+
+name = "Mykola"
+age = 14
+
+print(type(name))
+print(type(age))
+
+a = 10
+b = 4
+
+print(a + b)   # addition → 13
+print(a - b)   # subtraction → 7
+print(a * b)   # multiplication → 30
+print(a / b)   # division → 3.333...
+print(a // b)  # floor division → 3
+print(a % b)   # remainder → 1
+print(a ** b)  # exponent → 1000
+
+if age>= 18:
+    print("Adult")
+else:
+    print("Little boy/girl ")
+
+
+for i in range(10):
+    print(i)
+
+
+import random
+
+number = random.randint(1, 20)
+guess = 0
+attempts = 0
+max_attempts = 5
+
+print("Welcome to the guessing game!")
+print(f"You have {max_attempts} attempts to guess the number between 1 and 20.")
+
+while guess != number and attempts < max_attempts:
+    guess = int(input("Guess a number: "))
+    attempts += 1
+    remaining = max_attempts - attempts  # calculate remaining attempts
+
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print(f"🎉 Correct! You guessed it in {attempts} tries.")
+
+    if guess != number and remaining > 0:
+        print(f"You have {remaining} attempts left.\n")
+    elif remaining == 0 and guess != number:
+        print(f"💥 Game over! The number was {number}.")
+
+# import turtle
+
+# t = turtle.Turtle()
+# t.pensize(5)
+# t.pencolor("blue")
+# t.shape("circle")
+# t.penup()
+# t.right(200)
+# t.pendown()
+# t.forward(400)
+# t.left(200)
+# t.pencolor("red")
+# t.forward(600)
+# t.left(200)
+# t.pencolor("yellow")
+# t.forward(400)
+# t.left(160)
+# t.pencolor("green")
+# t.forward(150)
+# t.penup()
+# t.forward(100)
+# turtle.done()  
